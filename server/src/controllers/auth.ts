c4398c75e0870d794c606124c785502e36ae836a
asnganje <abd.nganje@gmail.com>
2026-02-19 16:34:14 +0300

    add crypto function for generating custom token

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express"
+import AuthVerificationTokenModel from "src/models/AuthVerificationToken";
 import UserModel from "src/models/User";
+import crypto from "crypto"
 
 export const sign_up: RequestHandler = async (req, res)=> {
   const {name, email, password} = req.body;
@@ -13,7 +15,10 @@ export const sign_up: RequestHandler = async (req, res)=> {
       res.status(401).json({status:"failed", message:"Email already in use!"})
     }
     const newUser = await UserModel.create({name, email, password})
-    res.json({status: "success", data: newUser})
+    const token = crypto.randomBytes(36).toString("hex")
+    await AuthVerificationTokenModel.create({owner: newUser._id, token})
+    const link = `http://localhost:3000/verify?id${newUser._id}&token=${token}`
+    res.send(link)
   } catch (error) {
     res.status(401).json({status:"fail", message:"Failed to create a user"})
   }
