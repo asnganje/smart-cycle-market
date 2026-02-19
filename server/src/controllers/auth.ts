0d95606492a8970efce7e08214900fffc4d7c3fb
asnganje <abd.nganje@gmail.com>
2026-02-19 16:21:17 +0300

    add user creation logic into auth signup

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,13 +1,22 @@
 import { RequestHandler } from "express"
+import UserModel from "src/models/User";
 
-export const sign_up: RequestHandler = (req, res)=> {
+export const sign_up: RequestHandler = async (req, res)=> {
   const {name, email, password} = req.body;
 
   if(!name) return res.status(422).json({message:"Name is missing!"})
   if(!email) return res.status(422).json({message:"Email is missing!"})
   if(!password) return res.status(422).json({message:"Password is missing!"})
-
-  res.json({message: "Auth register route"})
+  try {
+    const existingUser = await UserModel.findOne({email})
+    if (existingUser) {
+      res.status(401).json({status:"failed", message:"Email already in use!"})
+    }
+    const newUser = await UserModel.create({name, email, password})
+    res.json({status: "success", data: newUser})
+  } catch (error) {
+    res.status(401).json({status:"fail", message:"Failed to create a user"})
+  }
 }
 
 export const login: RequestHandler = (req, res)=> {
