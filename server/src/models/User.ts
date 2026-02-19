65036b1c990adecae09c0a631ca44df2f3911fa8
asnganje <abd.nganje@gmail.com>
2026-02-19 18:47:22 +0300

    add types to User Schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,22 +1,49 @@
-import { model, Schema } from "mongoose";
+import { Document, model, Schema } from "mongoose";
+import { hash, genSalt, compare } from "bcrypt";
 
+interface UserDocument extends Document {
+  name: string,
+  email: string,
+  password:string
+}
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
+interface Methods {
+  comparePassword(password: string): Promise<boolean>
+}
+
+const UserSchema = new Schema<UserDocument, {}, Methods>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email:{
-    type: String,
-    unique: true,
-    required: true
-  },
-  password:{
-    type: String,
-    required: true
+  { timestamps: true },
+);
+
+UserSchema.pre("save", async function () {
+  const salt = await genSalt(10);
+  if (this.isModified("password")) {
+    this.password = await hash(this.password, salt);
   }
-}, {timestamps: true})
+});
 
-const UserModel = model("User", UserSchema)
+UserSchema.methods.comparePassword = async function (
+  password
+) {
+  const isValid = await compare(password, this.password);
+  return isValid;
+};
+
+const UserModel = model("User", UserSchema);
 
 export default UserModel;
