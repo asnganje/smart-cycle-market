eb20cb1ed9b4dd2ee1b27aa9fe90533d8742804d
asnganje <abd.nganje@gmail.com>
2026-02-20 08:36:03 +0300

    pass validate middleware to individual routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { login, sign_up } from "src/controllers/auth";
+import { login, sign_up, verifyEmail } from "src/controllers/auth";
+import validate from "src/middleware/validator";
+import { NewUserSchema, VerifyTokenSchema } from "src/utils/validationSchema";
 
 const authRouter = express.Router();
 
-authRouter.route("/sign-up").post(sign_up);
+authRouter.route("/sign-up").post(validate(NewUserSchema), sign_up);
+authRouter.route("/verify").post(validate(VerifyTokenSchema), verifyEmail);
 authRouter.route("/login").post(login);
 
 export default authRouter;
