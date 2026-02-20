cce2de0c850a7d1007ab6fd7228693ab982a15f0
asnganje <abd.nganje@gmail.com>
2026-02-20 20:59:37 +0300

    add profile route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, sign_up, verifyEmail } from "src/controllers/auth";
+import { login, sendProfile, sign_up, verifyEmail } from "src/controllers/auth";
+import { isAuth } from "src/middleware/auth";
 import validate from "src/middleware/validator";
 import { NewUserSchema, VerifyTokenSchema } from "src/utils/validationSchema";
 
@@ -8,5 +9,6 @@ const authRouter = express.Router();
 authRouter.route("/sign-up").post(validate(NewUserSchema), sign_up);
 authRouter.route("/verify").post(validate(VerifyTokenSchema), verifyEmail);
 authRouter.route("/login").post(login);
+authRouter.route("/profile").get(isAuth, sendProfile);
 
 export default authRouter;
