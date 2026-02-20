fd7eaada1c0c0fd029beaee5631d09c09fb47e89
asnganje <abd.nganje@gmail.com>
2026-02-20 08:36:22 +0300

    add verifyTokenSchema for validation

--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import * as yup from "yup"
 
 const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
@@ -13,4 +14,15 @@ export const NewUserSchema = yup.object({
   name: yup.string().required("Name is required"),
   email:yup.string().email("Invalid email").required("Email is required"),
   password: yup.string().min(8, "Password should be at least eight characters long!").required("Password is required")
+}) 
+
+export const VerifyTokenSchema = yup.object({
+  id: yup.string().test({
+    name:"valid-id",
+    message: "Invalid user-id",
+    test:(value)=>{
+      return isValidObjectId(value)
+    }
+  }),
+  token:yup.string().required("Token is required")
 }) 
