1f91172e9454886218fc3adc1b97903a72b6fb52
asnganje <abd.nganje@gmail.com>
2026-02-21 10:15:40 +0300

    add verify token route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, sendProfile, sign_up, verifyEmail } from "src/controllers/auth";
+import { generateVerificationLink, login, sendProfile, sign_up, verifyEmail } from "src/controllers/auth";
 import { isAuth } from "src/middleware/auth";
 import validate from "src/middleware/validator";
 import { NewUserSchema, VerifyTokenSchema } from "src/utils/validationSchema";
@@ -9,6 +9,7 @@ const authRouter = express.Router();
 authRouter.route("/sign-up").post(validate(NewUserSchema), sign_up);
 authRouter.route("/verify").post(validate(VerifyTokenSchema), verifyEmail);
 authRouter.route("/login").post(login);
+authRouter.route("/verify-token").get(isAuth, generateVerificationLink);
 authRouter.route("/profile").get(isAuth, sendProfile);
 
 export default authRouter;
