906aac30f44c20d0afd2b3138eec17ce02d92ea0
asnganje <abd.nganje@gmail.com>
2026-02-22 06:55:28 +0300

    add reset password schema

--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -1,28 +1,43 @@
 import { isValidObjectId } from "mongoose";
-import * as yup from "yup"
+import * as yup from "yup";
 
-const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
-yup.addMethod(yup.string, 'email', function validateEmail(message) {
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+yup.addMethod(yup.string, "email", function validateEmail(message) {
   return this.matches(emailRegex, {
     message,
-    name: 'email',
+    name: "email",
     excludeEmptyString: true,
   });
 });
 
+const tokenAndId = {
+  id: yup.string().test({
+    name: "valid-id",
+    message: "Invalid user-id",
+    test: (value) => {
+      return isValidObjectId(value);
+    },
+  }),
+  token: yup.string().required("Token is required"),
+};
+
+const password = {
+  password: yup
+    .string()
+    .min(8, "Password should be at least eight characters long!")
+    .required("Password is required")
+}
 export const NewUserSchema = yup.object({
   name: yup.string().required("Name is required"),
-  email:yup.string().email("Invalid email").required("Email is required"),
-  password: yup.string().min(8, "Password should be at least eight characters long!").required("Password is required")
-}) 
+  email: yup.string().email("Invalid email").required("Email is required"),
+  ...password
+});
 
 export const VerifyTokenSchema = yup.object({
-  id: yup.string().test({
-    name:"valid-id",
-    message: "Invalid user-id",
-    test:(value)=>{
-      return isValidObjectId(value)
-    }
-  }),
-  token:yup.string().required("Token is required")
-}) 
+  ...tokenAndId,
+});
+
+export const ResetPasswordSchema = yup.object({
+  ...tokenAndId,
+  ...password
+});
