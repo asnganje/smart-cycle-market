296056d6a48fab17d6046547042fcf83d7914f5a
asnganje <abd.nganje@gmail.com>
2026-03-08 09:18:12 +0300

    add active color

--- scm-app/app/utils/colors.ts
+++ scm-app/app/utils/colors.ts
@@ -1,7 +1,8 @@
 export const Colors = {
-  primary:"#1C1C1F",
-  deActive:"#DCE4F2",
-  white:"#fff",
-  gray:"#696969",
-  backDrop:"rgba(255,255,255, 0.5)"
-}
+  primary: "#1C1C1F",
+  deActive: "#DCE4F2",
+  active: "#0081f1",
+  white: "#fff",
+  gray: "#696969",
+  backDrop: "rgba(255,255,255, 0.5)",
+};
