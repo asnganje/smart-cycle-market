54146d9e050ecf45be6946c7afd6936e415b5661
asnganje <abd.nganje@gmail.com>
2026-03-28 09:19:13 +0300

    allow user to enter message and display it on the log

--- scm-app/app/views/ChatWindow.tsx
+++ scm-app/app/views/ChatWindow.tsx
@@ -5,21 +5,42 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { AppStackParamList } from "./navigator/app/AppNavigator";
 import { FC } from "react";
 import PeerProfile from "../ui/PeerProfile";
-import { GiftedChat } from "react-native-gifted-chat";
+import { Avatar, GiftedChat } from "react-native-gifted-chat";
+import useAuth from "../hooks/useAuth";
 
 type ChatWindowProps = NativeStackScreenProps<AppStackParamList, "chatWindow">;
 const ChatWindow: FC<ChatWindowProps> = ({ route }) => {
   const { conversationId, peerProfile } = route.params;
+  const { authState } = useAuth();
+  const profile = authState.profile;
+
+  if(!profile) return null
 
   return (
-    <View>
+    <View style={styles.container}>
       <AppHeader
         backButton={<BackButton />}
-        center={<PeerProfile avatar={peerProfile.avatar} name={peerProfile.name}/>}
+        center={
+          <PeerProfile avatar={peerProfile.avatar} name={peerProfile.name} />
+        }
       />
-      <GiftedChat messages={[]}/>
+      <GiftedChat
+        messages={[]}
+        user={{
+          _id: profile.id,
+          name: profile?.name,
+          avatar: profile?.avatar
+        }}
+        onSend={(messages) => {
+          console.log(messages);
+        }}
+      />
     </View>
   );
 };
 export default ChatWindow;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
